378eeea39506b7adb113adfa51ada164cf2d21c4
ykcorpy <ykcorpy@gmail.com>
2012-10-03 10:56:59 +0300

    changes to ex15-5.py

--- Ex15/ex15-5.py
+++ Ex15/ex15-5.py
@@ -5,4 +5,5 @@ txt = open(filename)
 
 print txt.read()
 
-
+# argv is better than raw_input because you specify all relevant information on the command line. This way you don't have to wait for 
+# the input request to come.
