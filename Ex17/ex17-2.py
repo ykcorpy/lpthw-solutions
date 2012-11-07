addc8dd6971f8890eced14931eb91a1697d1b328
ykcorpy <ykcorpy@ykcorpy.(none)>
2012-11-07 20:43:07 +0200

    adding ex17-2.py

---
+++ Ex17/ex17-2.py
@@ -0,0 +1,19 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
